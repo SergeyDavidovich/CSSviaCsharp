21fdd82cd75e689fa47aee2644030cef9a1eca32
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-20 00:01:38 +0100

    no comments

---
+++ CSSviaCsharp/Shared/Colors.cs
@@ -0,0 +1,24 @@
+﻿namespace CSSviaCsharp.Shared.Themes
+{
+    public struct MaterialTheme
+    {
+        private string _name;
+        public MaterialTheme(string name)
+        {
+            _name = name;
+            ThemeColors = new();
+        }
+        public readonly List<Color> ThemeColors { get; init; }
+
+        public void AddColor(string color)
+        {
+            ThemeColors.Add(new Color() { });
+        }
+    }
+
+}
+public record struct Color
+{
+    public string? Value;
+}
+
